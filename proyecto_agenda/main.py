1929735ebd0032f27feec93c423ed80220a0efe3
rvalencia <rva.valencia@gmail.com>
2024-04-25 21:17:37 -0400

    v2 agenda

--- proyecto_agenda/main.py
+++ proyecto_agenda/main.py
@@ -8,35 +8,58 @@ Requerimientos:
 4. Que se puedan eliminar personas de la agenda
 5. Que se puedan buscar peronas en la agenda
 '''
+#Importamos OS para manipular consola
+import os  
+
 def buscarContacto(nombre, apellido, lista):    
     for c in lista:
         if c['nombre'] == nombre and c['apellido'] == apellido:
             return c
-    return None
+    return None 
+
+def buscarApellido(apellido, lista):
+    nueva_lista = []
+    for c in lista:
+        if c['apellido'] == apellido:
+            nueva_lista.append(c)
+    
+    return nueva_lista   
     
 agenda = [] #lista vacia
 
 while True:
+    #Esta instruccion limpia la pantalla
+    os.system('cls') 
+    
     print('==== Menu de opciones ====')
     print('1. Agregar contacto')
     print('2. Mostrar agenda')
     print('3. Eliminar contacto')
     print('4. Modificar contacto')
+    print('5. Buscar contacto')
     print('0. Salir') 
     
     opcion = input('Ingrese su opcion\n')
+    
+    #Esta instruccion limpia la pantalla
+    os.system('cls') 
     #Agregando personas
     if opcion == '1':
         print('==== Agregando Contacto ====')
         nombre = input('Ingrese su nombre\n').capitalize()
         apellido = input('Ingrese su apellido\n').capitalize() 
-        numero = int(input('Ingrese su numero de telefono\n')) #Validar que sean 8 numeros ingresados
-        
-        contacto = {'nombre':nombre, 'apellido':apellido, 'numero':numero}
-        
-        agenda.append(contacto)
-        
-        print(f'Se agrego el contacto {nombre} {apellido}')
+        try:
+            numero = int(input('Ingrese su numero de telefono\n')) #Validar que sean 8 numeros ingresados
+            
+            if len(str(numero)) != 8:
+                print('Deben ser 8 caracteres.')
+            else:        
+                contacto = {'nombre':nombre, 'apellido':apellido, 'numero':numero}
+                agenda.append(contacto)
+                print(f'Se agrego el contacto {nombre} {apellido}')
+        #Controlan que el programa se caiga
+        except:
+            print('No se pueden agregar letras como parte del numero >;(')
         
     elif opcion == '2':
         print('==== Lista contactos ====')
@@ -65,7 +88,7 @@ while True:
     elif opcion == '4':
         print('==== Modificar contacto ====')
         nombre = input('Ingrese nombre a modificar\n').capitalize()
-        apellido = input('Ingrese apellido a modificar\n').capitalize
+        apellido = input('Ingrese apellido a modificar\n').capitalize()
         
         resultado = buscarContacto(nombre, apellido, agenda)
         
@@ -77,11 +100,42 @@ while True:
             opcion = input('Ingrese su opcion\n')
             
             if opcion == '3':
-                numero = int(input('Ingrese nuevo numero\n'))  #Validar que sean 8 numeros ingresados
+                try:
+                    numero = int(input('Ingrese nuevo numero\n'))  #Validar que sean 8 numeros ingresados
+                    resultado['numero'] = numero
+                    print('Numero modificado')
+                except:
+                    print('No se pueden ingresar caracteres como numeros')
+            elif opcion == '2':
+                nuevo_apellido = input('Ingrese nuevo apellido\n').capitalize()
+                resultado['apellido'] = nuevo_apellido
                 
-                resultado['numero'] = numero
+            elif opcion == '3':
+                nuevo_nombre = input('Ingrese nuevo nombre\n').capitalize()
+                resultado['nombre'] = nuevo_nombre
                 
-                print('Numero modificado')
+            else:
+                print('Opcion no valida')
                 
         else:
             print('Contacto no encontrado')
+                
+    elif opcion == '5':
+        print('==== Buscar contacto ====')
+        apellido = input('Ingrese apellido a buscar\n').capitalize()
+        
+        lista_apellidos = buscarApellido(apellido, agenda)
+        
+        if len(lista_apellidos) == 0:
+            print('No existen contactos con ese apellido')
+        else:
+            print('==== Lista contatos ====')
+            for c in lista_apellidos:
+                print(f'Nombre: {c["nombre"]}')
+                print(f'Apellido: {c["apellido"]}')
+                print(f'Numero: {c["numero"]}')
+                print('-----------------------------------------------')
+        
+          
+    #Genera una pausa antes de reiniciar ciclo
+    input('Presione enter para continuar...')
